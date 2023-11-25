0d09764a79572410bc92535112b26159a3186fe9
vinyteles <vinitelesufg@gmail.com>
2023-11-25 00:28:17 -0300

    adding all hidden client functions

--- python/EmployeeClient.py
+++ python/EmployeeClient.py
@@ -16,25 +16,25 @@ def run(server):
         stub2 = EmployeeService_pb2_grpc.EmployeeServiceStub(channel2)
         print("passou do stub")
         print(str(stub2))
-        # # Query an employee's data
-        # response = stub.GetEmployeeDataFromID(EmployeeService_pb2.EmployeeID(id=101))
-        # print ('Employee\'s data: ' + str(response))
-        #
-        # # Add a new employee
-        # response = stub.CreateEmployee(EmployeeService_pb2.EmployeeData(id=301, name='Jose da Silva', title='Programmer'))
-        # print ('Added new employee ' + response.status)
-        #
-        # # Change an employee's title
-        # response = stub.UpdateEmployeeTitle(EmployeeService_pb2.EmployeeTitleUpdate(id=301, title='Senior Programmer'))
-        # print ('Updated employee ' + response.status)
-        #
-        # # Delete an employee
-        # response = stub.DeleteEmployee(EmployeeService_pb2.EmployeeID(id=201))
-        # print ('Deleted employee ' + response.status)
-        #
-        # # List all employees
-        # response = stub.ListAllEmployees(EmployeeService_pb2.EmptyMessage())
-        # print ('All employees: ' + str(response))
+        # Query an employee's data
+        response = stub2.GetEmployeeDataFromID(EmployeeService_pb2.EmployeeID(id=101))
+        print ('Employee\'s data: ' + str(response))
+
+        # Add a new employee
+        response = stub2.CreateEmployee(EmployeeService_pb2.EmployeeData(id=301, name='Jose da Silva', title='Programmer'))
+        print ('Added new employee ' + response.status)
+
+        # Change an employee's title
+        response = stub2.UpdateEmployeeTitle(EmployeeService_pb2.EmployeeTitleUpdate(id=301, title='Senior Programmer'))
+        print ('Updated employee ' + response.status)
+
+        # Delete an employee
+        response = stub2.DeleteEmployee(EmployeeService_pb2.EmployeeID(id=201))
+        print ('Deleted employee ' + response.status)
+
+        # List all employees
+        response = stub2.ListAllEmployees(EmployeeService_pb2.EmptyMessage())
+        print ('All employees: ' + str(response))
 
         # List all titles
         response = stub2.ListAllTitles(EmployeeService_pb2.EmptyMessage())
